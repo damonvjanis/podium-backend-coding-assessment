9929bbf1f5686dbc89d047045bdf6d65c77a3a67
Damon Janis <damonvjanis@gmail.com>
2019-05-03 20:38:57 -0600

    Create tests

---
+++ test/podium/order_test.exs
@@ -0,0 +1,61 @@
+defmodule Podium.OrderTest do
+  use ExUnit.Case, async: true
+
+  alias Podium.Order
+
+  doctest Order
+
+  setup_all do
+    # Read the test HTML file into a variable
+    html = File.read!("test/fixtures/html.txt")
+
+    # Make the variable available in test context
+    %{html: html}
+  end
+
+  describe "Order.get_positivity_score/1" do
+    test "gets the right score for a review", context do
+      review = context.html |> Podium.extract_review_data() |> List.first()
+
+      length = String.length(review.review)
+      {score, _review} = Order.get_positivity_score(review)
+
+      assert score == 0.01675977653631285
+      assert score * length == 3
+    end
+
+    test "returns the same review", context do
+      review_before = context.html |> Podium.extract_review_data() |> List.first()
+
+      {_score, review_after} = Order.get_positivity_score(review_before)
+
+      assert review_before == review_after
+    end
+  end
+
+  describe "Order.order_by_most_positive/1" do
+    test "first review after ordering is correct", context do
+      reviews = Podium.extract_review_data(context.html)
+
+      ordered = Order.order_by_most_positive(reviews)
+
+      most_positive = List.first(ordered)
+
+      assert most_positive.customer == "MsSharon"
+      assert most_positive.date == "April 23, 2019"
+      assert String.contains?(most_positive.review, "!!!!!!!!") == true
+    end
+
+    test "second review after ordering is correct", context do
+      reviews = Podium.extract_review_data(context.html)
+
+      ordered = Order.order_by_most_positive(reviews)
+
+      [_ | [second_most_positive | _]] = ordered
+
+      assert second_most_positive.customer == "DeEsta"
+      assert second_most_positive.date == "April 25, 2019"
+      assert String.contains?(second_most_positive.review, "NO high pressure") == true
+    end
+  end
+end
--- test/podium_test.exs
+++ test/podium_test.exs
@@ -1,8 +1,59 @@
 defmodule PodiumTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Podium
 
-  test "greets the world" do
-    assert Podium.hello() == :world
+  setup_all do
+    # Read the test HTML file into a variable
+    html = File.read!("test/fixtures/html.txt")
+
+    # Make the variable available in test context
+    %{html: html}
+  end
+
+  describe "Podium.get_reviews/0" do
+    @tag :integration
+    test "returns 3 well formed reviews maps" do
+      reviews = Podium.get_reviews()
+
+      assert length(reviews) == 3
+
+      for review <- reviews do
+        assert Map.has_key?(review, :review) == true
+        assert Map.has_key?(review, :customer) == true
+        assert Map.has_key?(review, :date) == true
+
+        assert is_nil(review.review) == false
+        assert is_nil(review.customer) == false
+        assert is_nil(review.date) == false
+      end
+    end
+  end
+
+  describe "Podium.get_html/1" do
+    @tag :integration
+    test "gets a string with an html declaration" do
+      html = Podium.get_html(1)
+
+      assert is_binary(html) == true
+      assert String.contains?(html, "<!DOCTYPE html>") == true
+    end
+  end
+
+  describe "Podium.extract_review_data/1" do
+    test "finds the appropriate data", context do
+      reviews = Podium.extract_review_data(context.html)
+
+      assert length(reviews) == 10
+
+      for review <- reviews do
+        assert Map.has_key?(review, :review) == true
+        assert Map.has_key?(review, :customer) == true
+        assert Map.has_key?(review, :date) == true
+
+        assert is_nil(review.review) == false
+        assert is_nil(review.customer) == false
+        assert is_nil(review.date) == false
+      end
+    end
   end
 end
